c8906efd80a552454ea9e93da6574dc5957287c6
Neissa Dorsinville <neissa@dorsinville.com>
2020-08-14 23:50:37 -0400

    Used GitHub API to get user info

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      newUser: ''
+    }
+  }
+  
+  getUser = (event) => {
+    event.preventDefault();
+    const user = event.target.elements.username.value;
+    if(user){
+      axios.get(`https://api.github.com/users/${user}`)
+           .then((res) => {
+            console.log(res)
+            const newUser = res.data.public_repos;
+            this.setState({ newUser })
+           })
+    }
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <SearchForm getUser={this.getUser} />
+        { this.state.newUser ? <p> Number of repos is:{ this.state.newUser } </p> : <p> Please enter a username </p> }
+      </div>
+    );
+  }
 }
 
 export default App;
